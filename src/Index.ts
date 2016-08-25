fa954bdcb28d6a85fa34db749d7b4d0af35cfaea
Murilo Kunze <murilokunze@hotmail.com>
2016-08-25 20:22:49 -0300

    version

--- src/Index.ts
+++ src/Index.ts
@@ -1,4 +1,4 @@
 export import DefaultViterbiTaggerFactory = require("./PartOfSpeechTagger/Factory/DefaultViterbiTaggerFactory");
 export import DefaultQuestionGeneratorFactory = require("./QuestionGenerator/Factory/DefaultQuestionGeneratorFactory");
 export import Text = require("./Text");
-// export import DefaultShiftReduceParserFactory = require("./Parser/Factory/DefaultShiftReduceParserFactory");
+export import DefaultCYKParserFactory = require("./Parser/Factory/DefaultCYKParserFactory");
