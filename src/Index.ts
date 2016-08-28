2030aeaed374c6f07ee416f332320090be8370e4
Murilo Kunze <murilokunze@hotmail.com>
2016-08-28 17:12:27 -0300

    export TextGrammarProvider

--- src/Index.ts
+++ src/Index.ts
@@ -2,3 +2,4 @@ export import DefaultViterbiTaggerFactory = require("./PartOfSpeechTagger/Factor
 export import DefaultQuestionGeneratorFactory = require("./QuestionGenerator/Factory/DefaultQuestionGeneratorFactory");
 export import Text = require("./Text");
 export import DefaultCYKParserFactory = require("./Parser/Factory/DefaultCYKParserFactory");
+export import TextGrammarProvider = require("./Parser/TextGrammarProvider");
