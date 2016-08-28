5a9ced472581d525ca81987da5fea361c2242dd0
Murilo Kunze <murilokunze@hotmail.com>
2016-08-28 17:17:05 -0300

    export GrammarReader

--- src/Index.ts
+++ src/Index.ts
@@ -4,3 +4,4 @@ export import Text = require("./Text");
 export import DefaultCYKParserFactory = require("./Parser/Factory/DefaultCYKParserFactory");
 export import TextGrammarProvider = require("./Parser/TextGrammarProvider");
 export import CYKParser = require("./Parser/CYKParser");
+export import GrammarReader = require("./GrammarReader");
