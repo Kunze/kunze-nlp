d97265b9f865d7c76721be78438085021085bc5c
Murilo Kunze <murilokunze@hotmail.com>
2016-09-06 09:48:09 -0300

    syntatic information

--- src/Main.ts
+++ src/Main.ts
@@ -2,12 +2,36 @@ import DefaultViterbiTaggerFactory = require("./PartOfSpeechTagger/Factory/Defau
 import DefaultQuestionGeneratorFactory = require("./QuestionGenerator/Factory/DefaultQuestionGeneratorFactory");
 import Text = require("./Text");
 import TaggedToken = require("./TaggedToken");
+import ParsedNode = require("./ParsedNode");
+
+// console.log(JSON.stringify(new ParsedNode("", "S", [
+//     new ParsedNode("A caça é ilegal mas o leão mata", "S", [
+//         new ParsedNode("A caça", "NP", [
+//             new ParsedNode("A", "ART"),
+//             new ParsedNode("caça", "N")
+//         ]),
+//         new ParsedNode("é ilegal mas o leão mata", "VP", [
+//             new ParsedNode("é ilegal", "VP", [
+//                 new ParsedNode("é", "V"),
+//                 new ParsedNode("ilegal", "ADJ"),
+//             ]),
+//             new ParsedNode("mas o leão mata", "CONJP", [
+//                 new ParsedNode("mas", "KC"),
+//                 new ParsedNode("o leão caça", "S", [
+//                     new ParsedNode("o leão", "NP"),
+//                     new ParsedNode("caça", "V")
+//                 ])
+//             ]),
+//         ])
+//     ]),
+//     new ParsedNode(".", "END")
+// ])));
 
 DefaultQuestionGeneratorFactory.create().then((questionGenerator) => {
     DefaultViterbiTaggerFactory.create().generateModel().then(tagger => {
         console.time("tagger");
 
-        let phrases = "Ele não gosta de trabalhar com programação de noite asasas uhhuhu.";
+        let phrases = "A caça é ilegal mas o leão caça.";
         let tokens = tagger.tag(phrases);
         let text = new Text(tokens);
 
