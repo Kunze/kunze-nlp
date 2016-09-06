17277c45753fe2cc6020e5b1e7884ec8d662c0b9
Murilo Kunze <murilokunze@hotmail.com>
2016-09-06 09:49:34 -0300

    export CorporaCYKParserFactory

--- src/Index.ts
+++ src/Index.ts
@@ -2,5 +2,6 @@ export import DefaultViterbiTaggerFactory = require("./PartOfSpeechTagger/Factor
 export import DefaultQuestionGeneratorFactory = require("./QuestionGenerator/Factory/DefaultQuestionGeneratorFactory");
 export import Text = require("./Text");
 export import DefaultCYKParserFactory = require("./Parser/Factory/DefaultCYKParserFactory");
+export import CorporaCYKParserFactory = require("./Parser/Factory/CorporaCYKParserFactory"); 
 export import CYKParser = require("./Parser/CYKParser");
 export import GrammarBuilder = require("./Parser/GrammarBuilder");
